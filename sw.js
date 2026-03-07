5ae1ce7b790a06856076fe7a9161780dfa69ce0f
jestty <121748586+jestty@users.noreply.github.com>
2026-03-07 22:30:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'quiz-app-v2'; // đổi version khi update
 
-const STATIC_FILES = ['index.html', 'style.css', 'script.js', 'manifest.json'];
+const STATIC_FILES = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
 
 // INSTALL
 self.addEventListener('install', (event) => {
@@ -53,3 +53,4 @@ self.addEventListener('fetch', (event) => {
     }),
   );
 });
+
